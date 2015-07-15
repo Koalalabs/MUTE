8f4867f148da70682bfc1d2df6dcfb7205609c6e
Flaxxas <C-Kock96@web.de>
2015-07-15 18:42:58 +0200

    GUI überarbeitet
    Grundgerüst erstellt, Pfeil-Icon geaddet

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/49/607e7aba0c2b00151b6dd99d9d61ad96
@@ -0,0 +1,9 @@
+package org.de.koalalabs.mute;
+
+public class StartMute {
+
+	public static void main(String[] args) {
+		MuteGui muteGui = new MuteGui();
+	}
+
+}
--- MUTE/src/org/de/koalalabs/mute/StartMute.java
+++ MUTE/src/org/de/koalalabs/mute/StartMute.java
@@ -3,7 +3,7 @@ package org.de.koalalabs.mute;
 public class StartMute {
 
 	public static void main(String[] args) {
-		MuteGui muteGui = new MuteGui();
+		new MuteGui();
 	}
 
 }
